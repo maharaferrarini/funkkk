e3041026b35d76551e4ddddf22696539e85ed034
maharaferrarini <90833156+maharaferrarini@users.noreply.github.com>
2021-09-17 09:17:21 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    radio.sendString("Mahara u")
+})
+radio.setGroup(30)
+radio.sendNumber(456)
+basic.forever(function () {
+	
+})
